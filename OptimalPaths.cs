a087ddd7faf3d7427af847c1f2957ed5d2425caa
binbinmods <binbinmods@gmail.com>
2025-01-09 18:21:59 -0500

    Alter color of optimal nodes

--- OptimalPaths.cs
+++ OptimalPaths.cs
@@ -6,6 +6,10 @@ using static Obeliskial_Essentials.Essentials;
 using System;
 using static OptimalPaths.CustomFunctions;
 using static OptimalPaths.Plugin;
+using UnityEngine.Windows.Speech;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
 
 // Make sure your namespace is the same everywhere
 namespace OptimalPaths
@@ -19,6 +23,7 @@ namespace OptimalPaths
         // Prefixes are executed before the original code, postfixes are executed after
         // Then you need to tell Harmony which method to patch.
 
+        static string[] optimalPath = ["sen_0","sen_1"];
         [HarmonyPrefix]
         [HarmonyPatch(typeof(AtOManager), nameof(AtOManager.AssignSingleGameNode))]
         public static void AssignSingleGameNodePrefix(ref AtOManager __instance, ref Node _node)
@@ -46,22 +51,55 @@ namespace OptimalPaths
             {
                 LogDebug(_node.nodeData.NodeId);
                 int[] outputPercents = new int[len];
-                LogDebug("after creation");
-                LogDebug($"len = {len}");
+                // LogDebug("after creation");
+                // LogDebug($"len = {len}");
                 outputPercents[len-1] = 100;
-                LogDebug("afterset");
-                LogDebug("NodeEvent Percents:" + string.Join(",", _node.nodeData.NodeEventPercent));
-                LogDebug("Output Percents:" + string.Join(",", outputPercents));
+                // LogDebug("afterset");
+                // LogDebug("NodeEvent Percents:" + string.Join(",", _node.nodeData.NodeEventPercent));
+                // LogDebug("Output Percents:" + string.Join(",", outputPercents));
 
                 _node.nodeData.NodeEventPercent = outputPercents;
             }
-
-
             return;
-
         }
 
+        // [HarmonyPostfix]
+        // [HarmonyPatch(typeof(MapManager), nameof(MapManager.BeginMap))]
+        // public static void BeginMapPostfix()
+        // {
+        //     LogInfo("BeginMap Postfix");
+        //     Dictionary<string, Node> mapNodeDict = MapManager.Instance.GetMapNodeDict();
 
+        // }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(Node), nameof(Node.AssignNode))]
+
+        public static void AssignNodePrefix(ref Node __instance)
+        {
+            LogInfo("AssignNodePrefix - Setting Optimal Nodes to Red");
+            if (optimalPath.Contains(__instance.nodeData.NodeId))
+            {
+                ParticleSystem nodeImageParticlesSystem = Traverse.Create(__instance).Field("nodeImageParticlesSystem").GetValue<ParticleSystem>();
+                if (nodeImageParticlesSystem==null)
+                {
+                    Transform nodeImageParticlesT = Traverse.Create(__instance).Field("nodeImageParticlesT").GetValue<Transform>();
+                    if (nodeImageParticlesT == null)
+                    {
+                        LogError("null nodeImageParticlesSystem");
+                        return;
+
+                    }
+                    nodeImageParticlesSystem = nodeImageParticlesT.GetComponent<ParticleSystem>();
+                }
+                
+                ParticleSystem.MainModule main = nodeImageParticlesSystem.main;
+                main.startColor = (ParticleSystem.MinMaxGradient) Color.red;
+                Traverse.Create(__instance).Field("nodeImageParticlesSystem").SetValue(nodeImageParticlesSystem);
+
+            }
+
+        }
 
     }
 }
